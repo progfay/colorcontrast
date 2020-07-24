4dec179aac563444210129df5fc6940201cb066d
progfay <shun1856shun@gmail.com>
2020-07-24 12:24:19 +0900

    feat: implement CalcContrastRatio

--- colorcontrast.go
+++ colorcontrast.go
@@ -1,8 +1,73 @@
 package colorcontrast
 
-import "image/color"
+import (
+	"image/color"
+	"math"
+)
 
 // CalcContrastRatio a
 func CalcContrastRatio(foreground, background color.Color) float64 {
-	return 0
+	bgOnWhite := alphaBlend(background, color.White)
+	bgOnBlack := alphaBlend(background, color.Black)
+
+	lWhite := getRelativeLuminance(bgOnWhite)
+	lBlack := getRelativeLuminance(bgOnBlack)
+	lFg := getRelativeLuminance(foreground)
+
+	if lWhite < lFg {
+		return getContrastRatioOpaque(foreground, bgOnWhite)
+	} else if lBlack > lFg {
+		return getContrastRatioOpaque(foreground, bgOnBlack)
+	} else {
+		return 1
+	}
+}
+
+func alphaBlend(foreground, background color.Color) color.RGBA {
+	fr, fg, fb, fa := foreground.RGBA()
+	br, bg, bb, _ := background.RGBA()
+
+	return color.RGBA{
+		R: uint8((fr*fa + br*(0xFFFF-fa)) / 65535),
+		G: uint8((fg*fa + bg*(0xFFFF-fa)) / 65535),
+		B: uint8((fb*fa + bb*(0xFFFF-fa)) / 65535),
+		A: 0xFF,
+	}
+}
+
+func getContrastRatioOpaque(foreground, background color.Color) float64 {
+	l1 := getRelativeLuminance(background)
+	l2 := getRelativeLuminance(alphaBlend(foreground, background))
+
+	// https://www.w3.org/TR/2008/REC-WCAG20-20081211/#contrast-ratiodef
+	return (math.Max(l1, l2) + 0.05) / (math.Min(l1, l2) + 0.05)
+}
+
+func getRelativeLuminance(c color.Color) float64 {
+	// https://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
+	cr, cg, cb, _ := c.RGBA()
+
+	r := float64(cr) / 65535
+	g := float64(cg) / 65535
+	b := float64(cb) / 65535
+
+	if r <= 0.03928 {
+					r = r / 12.92
+	} else {
+					r = math.Pow((r+0.055)/1.055, 2.4)
+	}
+
+	if g <= 0.03928 {
+					g = g / 12.92
+	} else {
+					g = math.Pow((g+0.055)/1.055, 2.4)
+	}
+
+	if b <= 0.03928 {
+					b = b / 12.92
+	} else {
+					b = math.Pow((b+0.055)/1.055, 2.4)
+	}
+
+	return 0.2126*r + 0.7152*g + 0.0722*b
 }
--- colorcontrast_test.go
+++ colorcontrast_test.go
@@ -3,6 +3,7 @@ package colorcontrast_test
 import (
 	"fmt"
 	"image/color"
+	"math"
 	"testing"
 
 	"github.com/progfay/colorcontrast"
@@ -38,6 +39,7 @@ func TestColorContrast(t *testing.T) {
 		b := fmt.Sprintf("#%02X%02X%02X%02X", br >> 8, bg >> 8, bb >> 8, ba >> 8)
 		t.Run(fmt.Sprintf("foreground: %s, background: %s", f, b), func(t *testing.T) {
 			got := colorcontrast.CalcContrastRatio(testcase.foreground, testcase.background)
+			got = math.Floor(got*100) / 100
 			if got != testcase.contrast {
 				t.Errorf("got %v, want %v", got, testcase.contrast)
 			}
